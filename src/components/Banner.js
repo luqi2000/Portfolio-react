09eeddb291f339c91730b7bddb8cb76c176fcc9d
Luqman Mohammad <123764610+luqi2000@users.noreply.github.com>
2023-09-24 17:38:10 +0100

    Added Banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,10 +1,94 @@
 import React from "react";
+import Image from "../assets/imgprofile.jpeg";
+
+import { FaGithub, FaYoutube, FaDribbble, FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa";
+
+import { TypeAnimation } from "react-type-animation";
+
+import { motion } from "framer-motion";
+
+import { fadeIn } from "../variants";
 
 const Banner = () => {
   return (
-    <div className="section" id="home">
-      Banner
-    </div>
+    <section className="min-h-[85vh] lg:min-h-[78vh] flex items-center" id="home">
+      <div className="container mx-auto">
+        <div className="flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12">
+          <div className="flex-1 text-center font-secondary lg:text-left">
+            <motion.h1
+              variants={fadeIn("down", 0.3)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="text-[55px] font-bold leading-[0.8] lg:text-[70px]">
+              LUQMAN <span>MOHAMMAD</span>
+            </motion.h1>
+            <motion.div
+              variants={fadeIn("down", 0.4)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="mb-6 text-[36px] lg:[60px] font-secondary font-semibold uppercase leading-[1]">
+              <span className="mr-4 text-white">I am a</span>
+              <TypeAnimation
+                sequence={["Front-end-Developer", 2000, "Back-end-Developer", 2000, "Full-stack-Developer", 2000]}
+                speed={50}
+                className="text-accent"
+                wrapper="span"
+                repeat={Infinity}
+              />
+            </motion.div>{" "}
+            <motion.p
+              variants={fadeIn("down", 0.5)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="mb-8 max-w-lg mx-auto lg:mx-0">
+              Hi, I recently completed a master of 6 month full time in Full Stack Development with success and
+              satisfaction and previously i have got a diploma in IT. With full of right to work in Uk I am looking for
+              a job in this sector.
+            </motion.p>
+            <motion.div
+              variants={fadeIn("down", 0.6)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0">
+              <button className="btn btn-lg">Contact me</button>
+              <a href="#" className="text-gradient btn-link">
+                My portfolio
+              </a>
+            </motion.div>
+            <motion.div
+              variants={fadeIn("down", 0.7)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0">
+              <a href="#">
+                <FaGithub />
+              </a>
+              <a href="#">
+                <FaLinkedin />
+              </a>
+              <a href="#">
+                <FaFacebook />
+              </a>
+              <a href="#">
+                <FaInstagram />
+              </a>
+            </motion.div>
+          </div>
+          <motion.div
+            variants={fadeIn("up", 0.5)}
+            initial="hidden"
+            whileInView={"show"}
+            className="max-w-[320px] hidden lg:flex flex-1">
+            <img src={Image} alt="photo profile" />
+          </motion.div>
+        </div>
+      </div>
+    </section>
   );
 };
 

cc129a4612fe82de8d6b026931991397acadfcfa
Luqman Mohammad <123764610+luqi2000@users.noreply.github.com>
2023-09-25 12:33:56 +0100

    contact section done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,41 @@
 import React from "react";
 
+import { motion } from "framer-motion";
+
+import { fadeIn } from "../variants";
+
 const Contact = () => {
   return (
-    <div className="section" id="contact">
-      Contact
-    </div>
+    <section className="py-16 lg:section" id="contact">
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row">
+          <div className="flex-1">
+            <div>
+              <h4 className="text-xl uppercase text-accent font-medium mb-2 tracking-wide">Get in touch!</h4>
+              <h2 className="text-[45px] lg:text[90px] leading-none mb-12">
+                Let's work <br /> together!
+              </h2>
+            </div>
+          </div>
+          <form className="flex-1 border rounded-2xl flex flex-col gap-y-6 pb-24 p-6 items-start">
+            <input
+              className="bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all"
+              type="text"
+              placeholder="Your name"
+            />
+            <input
+              className="bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all"
+              type="text"
+              placeholder="Your email"
+            />
+            <textarea
+              className="bg-transparent border-b py-12 outline-none w-full placeholder:text-white focus:border-accent transition-all resize-none mb-12"
+              placeholder="Your messege"></textarea>
+            <button className="btn btn-lg">Send message</button>
+          </form>
+        </div>
+      </div>
+    </section>
   );
 };
 

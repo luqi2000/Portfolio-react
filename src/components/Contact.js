7425f9b8dee122157f70d12604817e38ac11ef50
Luqman Mohammad <123764610+luqi2000@users.noreply.github.com>
2023-09-25 12:50:49 +0100

    added motion fadeIn on contact's section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,15 +9,25 @@ const Contact = () => {
     <section className="py-16 lg:section" id="contact">
       <div className="container mx-auto">
         <div className="flex flex-col lg:flex-row">
-          <div className="flex-1">
+          <motion.div
+            variants={fadeIn("right", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1 flex justify-start items-center">
             <div>
               <h4 className="text-xl uppercase text-accent font-medium mb-2 tracking-wide">Get in touch!</h4>
               <h2 className="text-[45px] lg:text[90px] leading-none mb-12">
                 Let's work <br /> together!
               </h2>
             </div>
-          </div>
-          <form className="flex-1 border rounded-2xl flex flex-col gap-y-6 pb-24 p-6 items-start">
+          </motion.div>
+          <motion.form
+            variants={fadeIn("left", 0.5)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1 border rounded-2xl flex flex-col gap-y-6 pb-24 p-6 items-start">
             <input
               className="bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all"
               type="text"
@@ -32,7 +42,7 @@ const Contact = () => {
               className="bg-transparent border-b py-12 outline-none w-full placeholder:text-white focus:border-accent transition-all resize-none mb-12"
               placeholder="Your messege"></textarea>
             <button className="btn btn-lg">Send message</button>
-          </form>
+          </motion.form>
         </div>
       </div>
     </section>

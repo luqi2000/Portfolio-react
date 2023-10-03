b6bf5a4a95d628f8388c5112c5c6639a816d69ec
Luqman Mohammad <123764610+luqi2000@users.noreply.github.com>
2023-10-03 22:14:06 +0100

    added emailJs

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,47 @@
-import React from "react";
-
+import React, { useState } from "react"; // Importa useState
+import emailjs from "emailjs-com";
 import { motion } from "framer-motion";
-
 import { fadeIn } from "../variants";
 
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    // Aggiungi uno stato per i dati del form
+    name: "",
+    email: "",
+    message: ""
+  });
+
+  const sendEmail = e => {
+    e.preventDefault();
+
+    const serviceID = "service_cjjuqlb";
+    const templateID = "template_l0vjdcd";
+    const userID = "YgXvC_S8paTXgIWsh";
+
+    emailjs
+      .send(serviceID, templateID, formData, userID)
+      .then(response => {
+        console.log("Email inviata con successo!", response);
+        // Resetta i dati del form dopo l'invio
+        setFormData({
+          name: "",
+          email: "",
+          message: ""
+        });
+      })
+      .catch(error => {
+        console.error("Errore nell'invio dell'email:", error);
+      });
+  };
+
+  const handleInputChange = e => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
   return (
     <section className="py-16 lg:section" id="contact">
       <div className="container mx-auto">
@@ -27,20 +64,31 @@ const Contact = () => {
             initial="hidden"
             whileInView={"show"}
             viewport={{ once: false, amount: 0.3 }}
-            className="flex-1 border rounded-2xl flex flex-col gap-y-6 pb-24 p-6 items-start">
+            className="flex-1 border rounded-2xl flex flex-col gap-y-6 pb-24 p-6 items-start"
+            onSubmit={sendEmail}>
             <input
               className="bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all"
               type="text"
               placeholder="Your name"
+              name="name"
+              value={formData.name} // Collega il valore del campo allo stato
+              onChange={handleInputChange} // Aggiungi gestore per l'input
             />
             <input
               className="bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all"
               type="text"
               placeholder="Your email"
+              name="email"
+              value={formData.email} // Collega il valore del campo allo stato
+              onChange={handleInputChange} // Aggiungi gestore per l'input
             />
             <textarea
               className="bg-transparent border-b py-12 outline-none w-full placeholder:text-white focus:border-accent transition-all resize-none mb-12"
-              placeholder="Your messege"></textarea>
+              placeholder="Your message"
+              name="message"
+              value={formData.message} // Collega il valore del campo allo stato
+              onChange={handleInputChange} // Aggiungi gestore per l'input
+            ></textarea>
             <button className="btn btn-lg">Send message</button>
           </motion.form>
         </div>
